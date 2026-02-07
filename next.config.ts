71125f1735c7c772c6f2f41644277b0237205314
ITMiquel <itvicoahorro4@gmail.com>
2026-02-07 15:33:45 +0100

    reestrctura

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   experimental: {
     // Needed for large multipart uploads when middleware is enabled.
     middlewareClientMaxBodySize: "1100mb"
